5733a0d7ab487797212ec5f9422c5f07e33e5da7
lisandromariano <145061399+lisandromariano@users.noreply.github.com>
2023-12-22 23:55:41 +0000

    no terminado

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -5,11 +5,18 @@ export const Navbar = () => {
 	return (
 		<nav className="navbar navbar-light bg-light mb-3">
 			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+				<span className="navbar-brand mb-0 h1">Starwars</span>
 			</Link>
 			<div className="ml-auto">
 				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
+					<div class="nav-item dropdown">
+					<button className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">Favoritos</button>
+					<ul class="dropdown-menu">
+					  <li><a class="dropdown-item justify-content-right" href="#">Action</a></li>
+					  <li><a class="dropdown-item justify-content-right" href="#">Another action</a></li>
+					  <li><a class="dropdown-item justify-content-right" href="#">Something else here</a></li>
+					</ul>
+					</div>
 				</Link>
 			</div>
 		</nav>
